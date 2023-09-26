4f7c8f85af67713ea0d642854a0699248654b2c8
nicolelfporca <113660629+nicolelfporca@users.noreply.github.com>
2023-09-26 15:35:36 +0800

    not complete

---
+++ Act03_MarianoPorca.py
@@ -0,0 +1,13 @@
+while True:
+    def SumofDigit(n):
+        total = 0
+        for i in str(n):
+            num = int(i)
+            total += num
+        return total
+    
+    number = int(input("Enter a Number: "))
+
+    result = SumofDigit(number)
+
+    print(f"{result}")
---
+++ Act04_MarianoPorca.py
@@ -0,0 +1,22 @@
+while True:
+    def perfectNumber(n):
+        if n <= 0:
+            return False
+
+        factors = [0]  
+        for i in range(1, n // 2 + 1):
+            if n % i == 0:
+                factors.append(i)
+                print(i)
+
+        totalFactors = sum(factors)
+
+        return totalFactors == n
+
+    num = int(input("Enter a number: "))
+    print("Factors: ")
+
+    if perfectNumber(num):
+        print(f"{num} is a perfect number.")
+    else:
+        print(f"{num} is not a perfect number.")
